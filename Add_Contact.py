20eb7d27b6daab91b4ee8b735a52bd345c759cac
vladimir1993 <kuzek1993@gmail.com>
2024-01-20 13:21:42 +0300

    refactor

--- Add_Contact.py
+++ Add_Contact.py
@@ -14,7 +14,41 @@ class AddContact(unittest.TestCase):
     
     def test_add_contact(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        self.home_page(wd)
+        self.login(wd)
+        self.click_add(wd)
+        self.create_contact(wd)
+        self.return_homepage(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        # logout
+        wd.find_element_by_link_text("Logout").click()
+
+    def return_homepage(self, wd):
+        # return to homepage
+        wd.find_element_by_link_text("home page").click()
+
+    def create_contact(self, wd):
+        # add atribute
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(u"Иванов")
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(u"Иван")
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys("1212")
+        wd.find_element_by_xpath("//div[@id='content']/form/input[20]").click()
+
+    def click_add(self, wd):
+        # click add new
+        wd.find_element_by_link_text("add new").click()
+
+    def login(self, wd):
+        # login
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
@@ -22,30 +56,14 @@ class AddContact(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//input[@value='Login']").click()
-        wd.find_element_by_link_text("add new").click()
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(u"Иванов")
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").click()
-        # ERROR: Caught exception [ERROR: Unsupported command [doubleClick | name=lastname | ]]
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(u"Иван")
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("1212")
-        wd.find_element_by_xpath("//div[@id='content']/form/input[20]").click()
-        wd.find_element_by_link_text("home page").click()
-        wd.find_element_by_link_text("Logout").click()
-    
+
+    def home_page(self, wd):
+        # open home page
+        wd.get("http://localhost/addressbook/")
+
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
         return True
 
     def tearDown(self):
