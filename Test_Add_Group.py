216a44052994520b56e982509282754471ae14ae
vladimir1993 <kuzek1993@gmail.com>
2024-01-22 22:23:04 +0300

    убрал wd

--- Test_Add_Group.py
+++ Test_Add_Group.py
@@ -11,34 +11,35 @@ class TestAddGroup(unittest.TestCase):
         self.wd.implicitly_wait(30)
     
     def test_add_group(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.open_group_page(wd)
-        self.create_new_group(wd)
-        self.add_description(wd, Group(name="1212", header="13131", footer="1414"))
-        self.creation_group(wd)
-        self.return_to_group(wd)
-        self.logout(wd)
+        self.open_home_page()
+        self.login(username="admin", password="secret")
+        self.open_group_page()
+        self.create_new_group()
+        self.add_description(Group(name="1212", header="13131", footer="1414"))
+        self.creation_group()
+        self.return_to_group()
+        self.logout()
 
     def test_add_empty_group(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.open_group_page(wd)
-        self.create_new_group(wd)
-        self.add_description(wd, Group(name="", header="", footer=""))
-        self.creation_group(wd)
-        self.return_to_group(wd)
-        self.logout(wd)
+        self.open_home_page()
+        self.login(username="admin", password="secret")
+        self.open_group_page()
+        self.create_new_group()
+        self.add_description(Group(name="", header="", footer=""))
+        self.creation_group()
+        self.return_to_group()
+        self.logout()
 
-    def logout(self, wd):
+    def logout(self):
+        wd = self.wd
         wd.find_element_by_link_text("Logout").click()
 
-    def return_to_group(self, wd):
+    def return_to_group(self):
+        wd = self.wd
         wd.find_element_by_link_text("group page").click()
 
-    def add_description(self, wd, group):
+    def add_description(self, group):
+        wd = self.wd
         # add_group_description
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
@@ -50,17 +51,21 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
 
-    def create_new_group(self, wd):
+    def create_new_group(self):
+        wd = self.wd
         wd.find_element_by_name("new").click()
 
-    def creation_group(self, wd):
+    def creation_group(self):
+        wd = self.wd
         wd.find_element_by_name("submit").click()
 
-    def open_group_page(self, wd):
+    def open_group_page(self):
+        wd = self.wd
         # open_groups_page
         wd.find_element_by_link_text("groups").click()
 
-    def login(self, wd, username, password):
+    def login(self, username, password):
+        wd = self.wd
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
@@ -70,7 +75,8 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_xpath("//*/text()[normalize-space(.)='']/parent::*").click()
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
-    def open_home_page(self, wd):
+    def open_home_page(self):
+        wd = self.wd
         wd.get("https://localhost/addressbook/")
 
     def tearDown(self):
