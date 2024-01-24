fccee22396a84758404a8cf3624b25924654eafb
vladimir1993 <kuzek1993@gmail.com>
2024-01-25 01:09:11 +0300

    Фикстура для контактов

--- Add_Contact.py
+++ Add_Contact.py
@@ -1,31 +1,28 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-import unittest, time, re
+import pytest
 from contact import Contact
 from appcontact import Appcontact
-class AddContact(unittest.TestCase):
-    def setUp(self):
-        self.app = Appcontact()
+
+
+@pytest.fixture
+def app(request):
+    fixture = Appcontact()
+    request.addfinalizer(fixture.destroy)
+    return fixture
     
-    def test_add_contact(self):
-        self.app.home_page()
-        self.app.login(user_name="admin", password="secret")
-        self.app.click_add()
-        self.app.create_contact(Contact(first_name=u"Иванов", last_name=u"Иван", phone="1212"))
-        self.app.return_homepage()
-        self.app.logout()
+def test_add_contact(app):
+    app.home_page()
+    app.login(user_name="admin", password="secret")
+    app.click_add()
+    app.create_contact(Contact(first_name=u"Иванов", last_name=u"Иван", phone="1212"))
+    app.return_homepage()
+    app.logout()
 
-    def test_add_contact_empty(self):
-        self.app.home_page()
-        self.app.login(user_name="admin", password="secret")
-        self.app.click_add()
-        self.app.create_contact(Contact(first_name=u"", last_name=u"", phone=""))
-        self.app.return_homepage()
-        self.app.logout()
+def test_add_contact_empty(app):
+    app.home_page()
+    app.login(user_name="admin", password="secret")
+    app.click_add()
+    app.create_contact(Contact(first_name=u"", last_name=u"", phone=""))
+    app.return_homepage()
+    app.logout()
 
-    def tearDown(self):
-        self.app.destroy()
-
-if __name__ == "__main__":
-    unittest.main()
