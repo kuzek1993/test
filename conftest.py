942ceb608af37665779221b33f1fa33228cd2b66
vladimir1993 <kuzek1993@gmail.com>
2024-01-26 01:01:53 +0300

    общая фикстура

--- conftest.py
+++ conftest.py
@@ -1,8 +1,7 @@
 import pytest
-from fixture.application import Application
+from fixture.test_application import Application
 
-
-@pytest.fixture(scope="session")
+@pytest.fixture(scope = "session")
 def app(request):
     fixture = Application()
     request.addfinalizer(fixture.destroy)
--- test/test_Add_Group.py
+++ test/test_Add_Group.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
 from model.group import Group
-from fixture.test_application import Application
+from fixture.application import Application
 
 @pytest.fixture
 def app(request):
