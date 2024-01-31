f8ec3a5f35e016d1b068f792899d1877cf5e3c2c
vladimir1993 <kuzek1993@gmail.com>
2024-01-31 23:22:28 +0300

    Добавил тест обновления контакта

--- fixture/contact.py
+++ fixture/contact.py
@@ -27,7 +27,20 @@ class ContactHelper:
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_xpath("//input[@value='Delete']").click()
 
-
+    def change_first_contact(self, contact):
+        wd = self.app.wd
+        wd.find_element_by_xpath("//img[@alt='Edit']").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.first_name)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.last_name)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.phone)
+        wd.find_element_by_name("update").click()
 
     def click_add(self):
         # click add new
