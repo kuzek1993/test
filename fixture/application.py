8024b1da02c5fcc83e87203bcfff8553d64674c9
vladimir1993 <kuzek1993@gmail.com>
2024-02-07 01:18:56 +0300

    Умная авторизация

--- fixture/application.py
+++ fixture/application.py
@@ -7,7 +7,7 @@ class Application:
 
     def __init__(self):
         self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(15)
+        self.wd.implicitly_wait(6)
         self.wd.get("https://localhost/addressbook/")
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
--- fixture/session.py
+++ fixture/session.py
@@ -16,3 +16,26 @@ class SessionHelper:
     def logout(self):
         wd = self.app.wd
         wd.find_element_by_link_text("Logout").click()
+
+    def ensure_logout(self):
+        wd = self.app.wd
+        if self.logget_in():
+            self.logout()
+
+    def logget_in_as(self, username):
+        wd = self.app.wd
+        return wd.find_element_by_xpath("//div[@id='top']/form/b").text == "("+username+")"
+
+
+    def logget_in(self):
+        wd = self.app.wd
+        return len(wd.find_element_by_link_text("Logout")) > 0
+
+    def ensure_login(self, username, password):
+        wd = self.app.wd
+        if self.logget_in():
+            if self.logget_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username, password)
