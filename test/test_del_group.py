97513485c1a42ce805b51be071c1adf88931f1fd
vladimir1993 <kuzek1993@gmail.com>
2024-02-07 18:47:35 +0300

    проверка для удаления первой группы

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,6 +1,9 @@
 
 import time
+from model.group import Group
 def test_delete_first_group(app):
+    if app.group.count() == 0:
+        app.group.create_new(Group(name="для удаления"))
     app.group.delete_first_group()
     app.group.return_to_group()
     time.sleep(1)
