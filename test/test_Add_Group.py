7ce5d3bb5e44e7b455cd0b7e6851cde8440037de
vladimir1993 <kuzek1993@gmail.com>
2024-02-10 22:39:30 +0300

    получаем список групп

--- test/test_Add_Group.py
+++ test/test_Add_Group.py
@@ -5,12 +5,18 @@ from model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create_new(Group(name="1212", header="13131", footer="1414"))
     app.group.return_to_group()
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
     time.sleep(1)
 
 
 def test_add_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create_new(Group(name="", header="", footer=""))
     app.group.return_to_group()
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
     time.sleep(1)
--- test/test_change_group.py
+++ test/test_change_group.py
@@ -4,18 +4,27 @@ from model.group import Group
 
 
 def test_modify_group_name(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create_new(Group(name="модификация"))
     app.group.change_description(Group(name="new group"))
     app.group.return_to_group()
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
     time.sleep(1)
 
 def test_modify_group_header(app):
+    old_groups = app.group.get_group_list()
     app.group.change_description(Group(header="new header"))
     app.group.return_to_group()
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
     time.sleep(1)
 
 def test_modify_group_footer(app):
+    old_groups = app.group.get_group_list()
     app.group.change_description(Group(footer="1414"))
     app.group.return_to_group()
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
     time.sleep(1)
