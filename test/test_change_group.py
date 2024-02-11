331785489d97a80f4e1124576911f4de60b0acfd
vladimir1993 <kuzek1993@gmail.com>
2024-02-11 20:28:54 +0300

    сравнение списков для контактов

--- test/test_change_group.py
+++ test/test_change_group.py
@@ -7,10 +7,14 @@ def test_modify_group_name(app):
     old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create_new(Group(name="модификация"))
-    app.group.change_description(Group(name="new group"))
+    group = Group(name="new group")
+    group.id = old_groups[0].id
+    app.group.change_description(group)
     app.group.return_to_group()
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
     time.sleep(1)
 
 def test_modify_group_header(app):
