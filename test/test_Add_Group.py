cd1342afc77e3680ef9c57db6f6c777ef85105c3
vladimir1993 <kuzek1993@gmail.com>
2024-02-21 22:50:56 +0300

    параметризация для групп

--- test/test_Add_Group.py
+++ test/test_Add_Group.py
@@ -1,12 +1,27 @@
 # -*- coding: utf-8 -*-
 import time
 
+import pytest
+import random
+import string
+
 from model.group import Group
 
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-def test_add_group(app):
+testdata = [
+            Group(name=name, header=header, footer=footer)
+            for name in ["", random_string("name", 10)]
+            for header in ["", random_string("header", 20)]
+            for footer in ["", random_string("footer", 20)]
+    ]
+
+
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_add_group(app, group):
     old_groups = app.group.get_group_list()
-    group = Group(name="1212", header="13131", footer="1414")
     app.group.create_new(group)
     app.group.return_to_group()
     new_groups = app.group.get_group_list()
@@ -16,13 +31,13 @@ def test_add_group(app):
     time.sleep(1)
 
 
-def test_add_empty_group(app):
-    old_groups = app.group.get_group_list()
-    group = Group(name="", header="", footer="")
-    app.group.create_new(group)
-    app.group.return_to_group()
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-    time.sleep(1)
+#def test_add_empty_group(app):
+    #old_groups = app.group.get_group_list()
+    #group = Group(name="", header="", footer="")
+    #app.group.create_new(group)
+    #app.group.return_to_group()
+    #new_groups = app.group.get_group_list()
+    #assert len(old_groups) + 1 == len(new_groups)
+    #old_groups.append(group)
+    #assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    #time.sleep(1)
