781b6c4cf3500ae7674c95092715f2b248be1bc2
vladimir1993 <kuzek1993@gmail.com>
2024-02-22 11:36:03 +0300

    опции командной строки

--- fixture/application.py
+++ fixture/application.py
@@ -5,12 +5,20 @@ from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox()
-        self.wd.get("https://localhost/addressbook/")
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+        self.wd.get(base_url)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
