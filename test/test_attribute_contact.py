74bbcd08171a061f12411ddc5bb313f9d13e53df
vladimir1993 <kuzek1993@gmail.com>
2024-03-12 22:51:23 +0300

    Сравнение атрибутов контактов ui/бд

--- test/test_attribute_contact.py
+++ test/test_attribute_contact.py
@@ -1,13 +1,20 @@
 import re
+from model.contact import Contact
 
-def test_attribute_on_homepage(app):
-    attribute_from_homepage = app.contact.get_contact_list() [0]
-    attribute_from_editpage = app.contact.get_contact_info_from_edit_page(0)
-    assert attribute_from_homepage.all_email_from_home_page == merge_email_on_homepage(attribute_from_editpage)
-    assert attribute_from_homepage.all_phones_from_home_page == merge_phones_on_homepage(attribute_from_editpage)
-    assert attribute_from_homepage.first_name == (attribute_from_editpage.first_name)
-    assert attribute_from_homepage.last_name == (attribute_from_editpage.last_name)
-    assert attribute_from_homepage.adress == (attribute_from_editpage.adress)
+
+def test_attribute_on_homepage(app, db):
+
+
+    attribute_ui_from_homepage = app.contact.get_contact_list()
+    def clean(contact):
+        return Contact(id=contact.id, first_name=contact.first_name, last_name=contact.last_name, mobilephone=contact.mobilephone, email1=contact.email1,
+                       email2=contact.email2, email3=contact.email3, adress=contact.adress, homephone=contact.homephone, workphone=contact.workphone)
+    attribute_from_db = map(clean, db.get_contact_list())
+    assert sorted(attribute_ui_from_homepage, key=Contact.id_or_max) == sorted(attribute_from_db, key=Contact.id_or_max)
+    #assert attribute_ui_from_homepage.all_phones_from_home_page == attribute_from_db
+    #assert attribute_ui_from_homepage.first_name == attribute_from_db
+    #assert attribute_ui_from_homepage.last_name == attribute_from_db
+    #assert attribute_ui_from_homepage.adress == attribute_from_db
 
 def clear(s):
     return re.sub("[() -]", "", s)
@@ -23,9 +30,9 @@ def merge_phones_on_homepage(contact):
                                        [contact.homephone, contact.mobilephone, contact.workphone, ]))))
 
 
-def test_phones_on_contact_view_page(app):
-    contact_from_view_page = app.contact.get_contact_from_view_page(0)
-    contact_from_editpage = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_view_page.homephone == (contact_from_editpage.homephone)
-    assert contact_from_view_page.mobilephone == (contact_from_editpage.mobilephone)
-    assert contact_from_view_page.workphone == (contact_from_editpage.workphone)
+#def test_phones_on_contact_view_page(app):
+    #contact_from_view_page = app.contact.get_contact_from_view_page(0)
+    #contact_from_editpage = app.contact.get_contact_info_from_edit_page(0)
+    #assert contact_from_view_page.homephone == (contact_from_editpage.homephone)
+    #assert contact_from_view_page.mobilephone == (contact_from_editpage.mobilephone)
+    #assert contact_from_view_page.workphone == (contact_from_editpage.workphone)
